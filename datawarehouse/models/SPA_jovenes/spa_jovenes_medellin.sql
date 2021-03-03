9097e01740d101e259ce308a029c014c175b5dbd
alex <alex-12-04@hotmail.com>
2021-03-03 10:21:56 -0500

    se suben cambios modelo covid y se adiciona modelo SPA jovenes medellin

---
+++ datawarehouse/models/SPA_jovenes/spa_jovenes_medellin.sql
@@ -0,0 +1,1 @@
+select * from {{ var("schema") }}.data_covid
--- datawarehouse/models/covid/covid_colombia.sql
+++ datawarehouse/models/covid/covid_colombia.sql
@@ -1,12 +1,72 @@
 select
        cast("ID de caso" as varchar(10)) as id,
-       substr(trim("Fecha de recuperación"), 0, 10) as fecha_reporte,
-       substr(trim("Fecha de notificación"), 0, 10) as fecha_notificacion,
-       substr(trim("Fecha de diagnóstico"), 0, 10) as fecha_diagnóstico,
-       substr(trim("Fecha de inicio de síntomas"), 0, 10) as fecha_síntomas,
-       substr(trim("Fecha de recuperación"), 0, 10) as fecha_recuperación,
-       substr(trim("Fecha de muerte"), 0, 10) as fecha_muerte,
-       cast("Tipo de recuperación" as varchar(10)),
+       case
+        WHEN "Fecha de recuperación" != null or "Fecha de recuperación" != '' then
+           TO_DATE(CONCAT(split_part(split_part(trim("Fecha de recuperación"), '/', 3), ' ', 1),
+           case
+            WHEN cast(split_part(trim("Fecha de recuperación"), '/', 2) as integer) <= 9 then concat('0', split_part(trim("Fecha de recuperación"), '/', 2)) else split_part(trim("Fecha de recuperación"), '/', 2)
+            end,
+           case
+            WHEN cast(split_part(trim("Fecha de recuperación"), '/', 1) as integer) <= 9 then concat('0', split_part(trim("Fecha de recuperación"), '/', 1)) else split_part(trim("Fecha de recuperación"), '/', 1)
+            end), 'YYYYMMDD')
+        else null
+        end as fecha_recuperacion,
+       case
+        WHEN "Fecha de notificación" != null or "Fecha de notificación" != '' then
+           TO_DATE(CONCAT(split_part(split_part(trim("Fecha de notificación"), '/', 3), ' ', 1),
+           case
+            WHEN cast(split_part(trim("Fecha de notificación"), '/', 2) as integer) <= 9 then concat('0', split_part(trim("Fecha de notificación"), '/', 2)) else split_part(trim("Fecha de notificación"), '/', 2)
+            end,
+           case
+            WHEN cast(split_part(trim("Fecha de notificación"), '/', 1) as integer) <= 9 then concat('0', split_part(trim("Fecha de notificación"), '/', 1)) else split_part(trim("Fecha de notificación"), '/', 1)
+            end), 'YYYYMMDD')
+        else null
+        end as fecha_notificacion,
+       case
+        WHEN "Fecha de diagnóstico" != null or "Fecha de diagnóstico" != '' then
+           TO_DATE(CONCAT(split_part(split_part(trim("Fecha de diagnóstico"), '/', 3), ' ', 1),
+           case
+            WHEN cast(split_part(trim("Fecha de diagnóstico"), '/', 2) as integer) <= 9 then concat('0', split_part(trim("Fecha de diagnóstico"), '/', 2)) else split_part(trim("Fecha de diagnóstico"), '/', 2)
+            end,
+           case
+            WHEN cast(split_part(trim("Fecha de diagnóstico"), '/', 1) as integer) <= 9 then concat('0', split_part(trim("Fecha de diagnóstico"), '/', 1)) else split_part(trim("Fecha de diagnóstico"), '/', 1)
+            end), 'YYYYMMDD')
+        else null
+        end as fecha_diagnóstico,
+       case
+        WHEN "Fecha de inicio de síntomas" != null or "Fecha de inicio de síntomas" != '' then
+           TO_DATE(CONCAT(split_part(split_part(trim("Fecha de inicio de síntomas"), '/', 3), ' ', 1),
+           case
+            WHEN cast(split_part(trim("Fecha de inicio de síntomas"), '/', 2) as integer) <= 9 then concat('0', split_part(trim("Fecha de inicio de síntomas"), '/', 2)) else split_part(trim("Fecha de inicio de síntomas"), '/', 2)
+            end,
+           case
+            WHEN cast(split_part(trim("Fecha de inicio de síntomas"), '/', 1) as integer) <= 9 then concat('0', split_part(trim("Fecha de inicio de síntomas"), '/', 1)) else split_part(trim("Fecha de inicio de síntomas"), '/', 1)
+            end), 'YYYYMMDD')
+        else null
+        end as fecha_síntomas,
+       case
+        WHEN "Fecha de recuperación" != null or "Fecha de recuperación" != '' then
+           TO_DATE(CONCAT(split_part(split_part(trim("Fecha de recuperación"), '/', 3), ' ', 1),
+           case
+            WHEN cast(split_part(trim("Fecha de recuperación"), '/', 2) as integer) <= 9 then concat('0', split_part(trim("Fecha de recuperación"), '/', 2)) else split_part(trim("Fecha de recuperación"), '/', 2)
+            end,
+           case
+            WHEN cast(split_part(trim("Fecha de recuperación"), '/', 1) as integer) <= 9 then concat('0', split_part(trim("Fecha de recuperación"), '/', 1)) else split_part(trim("Fecha de recuperación"), '/', 1)
+            end), 'YYYYMMDD')
+        else null
+        end as fecha_recuperación,
+       case
+        WHEN "Fecha de muerte" != null or "Fecha de muerte" != '' then
+           TO_DATE(CONCAT(split_part(split_part(trim("Fecha de muerte"), '/', 3), ' ', 1),
+           case
+            WHEN cast(split_part(trim("Fecha de muerte"), '/', 2) as integer) <= 9 then concat('0', split_part(trim("Fecha de muerte"), '/', 2)) else split_part(trim("Fecha de muerte"), '/', 2)
+            end,
+           case
+            WHEN cast(split_part(trim("Fecha de muerte"), '/', 1) as integer) <= 9 then concat('0', split_part(trim("Fecha de muerte"), '/', 1)) else split_part(trim("Fecha de muerte"), '/', 1)
+            end), 'YYYYMMDD')
+        else null
+        end as fecha_muerte,
+       cast("Tipo de recuperación" as varchar(10)) as tipo_de_recuperación,
        cast("Código DIVIPOLA departamento" as int) as codigo_departamento,
        cast("Código DIVIPOLA municipio" as int) as codigo_municipio,
        cast("Código ISO del país" as int) as codigo_pais,
@@ -14,11 +74,12 @@ select
        "Nombre municipio" as municipio,
        "Nombre del país" as pais,
        cast("Edad" as int) as edad,
+       cast("Sexo" as varchar(1)) as sexo,
        "Tipo de contagio" as tipo_contagio,
        "Tipo de recuperación" as tipo_recuperacion,
        "Estado" as estado_gravedad,
        "Ubicación del caso" as ubicacion,
-       "Recuperado" estado_actual,
+       case when "Recuperado" = "fallecido" then 'fallecido no covid' else "Recuperado" end as estado_actual,
        case
            WHEN "Recuperado" = 'Recuperado' then 'si'
            ELSE 'no'
